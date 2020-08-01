9d3e3d421f1bd7ed29971d0731574cd288ea0823
sterang <hamil@unicauca.edu.co>
2020-08-01 12:25:25 -0500

    Video conference con jitsi

--- src/main.js
+++ src/main.js
@@ -2,9 +2,16 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
+import LoadScript from 'vue-plugin-load-script';
 Vue.config.productionTip = false;
-
+Vue.use(LoadScript);
+Vue.loadScript("https://meet.jit.si/external_api.js")
+  .then(() => {
+    console.log("Cargado")
+  })
+  .catch(() => {
+    // Failed to fetch script
+  });
 new Vue({
   router,
   store,
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,63 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div id="jitsi-container"></div>
   </div>
 </template>
+ <script src='https://meet.jit.si/external_api.js'></script>
+<script>
+//import JitsiMeetExternalAPI from "../store/metting";
+export default {
+  data() {
+    return {
+      room: "test romm",
+      username: "rabie"
+    };
+  },
+  methods: {
+    startConference() {
+      var _this = this;
+      try {
+        const domain = "meet.jit.si";
+        const options = {
+          audioInput: "<deviceLabel>",
+          audioOutput: "<deviceLabel>",
+          videoInput: "<deviceLabel>",
+          roomName: this.room,
+          height: 500,
+          parentNode: document.getElementById("jitsi-container"),
+          interfaceConfigOverwrite: {
+            filmStripOnly: false,
+            SHOW_JITSI_WATERMARK: false
+          },
+          configOverwrite: {
+            disableSimulcast: false
+          }
+        };
+
+        this.api = new JitsiMeetExternalAPI(domain, options);
+        this.api.addEventListener("videoConferenceJoined", () => {
+          console.log("Local User Joined");
+
+          _this.api.executeCommand("displayName", _this.username);
+        });
+      } catch (error) {
+        console.error("Failed to load Jitsi API", error);
+      }
+    },
+    openRoom() {
+      // verify the JitsiMeetExternalAPI constructor is added to the global..
+      if (window.JitsiMeetExternalAPI) {
+        var person = prompt("Please enter your name:", "Rabie");
+        if (person != null || person != "") this.username = person;
+        var room = prompt("Please enter your room:", "Test Room");
+        if (room != null || room != "") this.room = room;
+        this.startConference();
+      } else alert("Jitsi Meet API script not loaded");
+    }
+  },
+
+  mounted() {
+    this.openRoom();
+  }
+};
+</script>
